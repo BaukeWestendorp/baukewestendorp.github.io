4ed9e42ba30e738e6a40d8d1ea046b89f446d8d6
BaukeWestendorp <baukewestendorp@gmail.com>
2022-12-03 18:24:47 +0100

    Create basic website design

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,14 @@
+<nav>
+	<ul>
+		<li><a href="/">expositie</a></li>
+		<li><a href="/over">over</a></li>
+	</ul>
+</nav>
+
+<style>
+	ul {
+		list-style: none;
+		display: flex;
+		gap: 1rem;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '$lib/styles/main.css';
+	import '$lib/styles/typography.css';
+
+	import NavBar from '$lib/components/NavBar.svelte';
+</script>
+
+<div class="layout">
+	<h1>Bauke Westendorp</h1>
+
+	<NavBar />
+
+	<slot />
+</div>
+
+<style>
+	.layout {
+		max-width: 34rem;
+		padding: 1rem;
+		margin: auto;
+	}
+</style>
