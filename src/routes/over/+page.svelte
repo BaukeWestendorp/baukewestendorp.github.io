c328c5ce47f807701d5a3054d9848984fbe2913e
BaukeWestendorp <baukewestendorp@gmail.com>
2022-12-06 23:21:26 +0100

    Add Over page content

--- src/routes/over/+page.svelte
+++ src/routes/over/+page.svelte
@@ -3,3 +3,19 @@
 </svelte:head>
 
 <h2>Over</h2>
+
+<div class="over">
+	<img src="/pages/over/bauke.jpg" alt="Bauke" />
+	<p>
+		Je had hoogstwaarschijnlijk al zo'n vermoeden: ik ben Bauke Westendorp. Al heel mijn leven woon
+		in het weinigzijdige maar vertrouwde Drachten, alhoewel mijn vroegtijdige studentenhartje al een
+		tijd een oogje op het prachtige Groningen heeft. Ik kan me onvoorwaardelijk vermaken met muziek,
+		foto, video, kunst en voornamelijk programmeren met een mespuntje zwarte peper.
+	</p>
+</div>
+
+<style>
+	img {
+		width: 100%;
+	}
+</style>
