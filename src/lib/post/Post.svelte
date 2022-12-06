7846730e62470801a0278d07fe4e24ba8d535f20
BaukeWestendorp <baukewestendorp@gmail.com>
2022-12-06 23:46:06 +0100

    Enlargen image in Expositie when clicked on

--- src/lib/post/Post.svelte
+++ src/lib/post/Post.svelte
@@ -2,13 +2,60 @@
 	import type { Post } from './types';
 
 	export let post: Post;
+
+	let isSelected = false;
 </script>
 
-<img src="/posts/images/{post.image}" alt={post.image} />
+<img
+	class="thumbnail"
+	src="/posts/images/{post.image}"
+	alt={post.image}
+	on:click={() => (isSelected = true)}
+/>
+
+{#if isSelected}
+	<div class="enlargend" on:click={() => (isSelected = false)}>
+		<img src="/posts/images/{post.image}" alt={post.image} />
+		<button>Close</button>
+	</div>
+{/if}
 
 <style>
-	img {
+	.thumbnail {
 		width: 100%;
 		height: 100%;
+
+		cursor: pointer;
+		transition: 0.1s scale;
+	}
+
+	@media not screen and (hover: none) and (pointer: coarse) {
+		.thumbnail:hover {
+			opacity: 0.8;
+			scale: 0.95;
+		}
+	}
+
+	.enlargend {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+
+		padding: 1rem;
+
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+
+		background: #000d;
+	}
+
+	.enlargend img {
+		width: 100%;
+		max-width: 80rem;
 	}
 </style>
