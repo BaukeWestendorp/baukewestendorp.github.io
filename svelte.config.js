67b18220c94148b22babcea99e15d201d5af09df
BaukeWestendorp <baukewestendorp@gmail.com>
2023-02-14 22:52:44 +0100

    Remove unnecessary variable

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
