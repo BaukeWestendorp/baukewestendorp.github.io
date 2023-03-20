8f6cd2931d82ff41b4bdc2cd05a98325ed031cdf
BaukeWestendorp <baukewestendorp@gmail.com>
2023-03-20 15:36:25 -0400

    over: Remove projects list from /over page

--- docs/_app/immutable/chunks/3-ee49c19d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/over/_page.svelte-7e034daf.js";export{t as component};
--- src/routes/over/+page.svelte
+++ src/routes/over/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import Link from '$lib/link/Link.svelte';
 </script>
 
 <svelte:head>
@@ -17,14 +16,6 @@
 		tijd een oogje op het prachtige Groningen heeft. Ik kan me onvoorwaardelijk vermaken met muziek,
 		foto, video, kunst en voornamelijk programmeren met een mespuntje zwarte peper.
 	</p>
-
-	<h3>Coole projecten die mij aanspreken of waar ik mee te maken heb</h3>
-	<ul>
-		<li><Link openInNewTab extern href="https://dwangschematiek.nl">dwangschematiek.nl</Link></li>
-		<li>
-			<Link openInNewTab extern href="https://impossiblerobotics.nl">impossiblerobotics.nl</Link>
-		</li>
-	</ul>
 </div>
 
 <style>
@@ -34,9 +25,5 @@
 
 	p {
 		margin-top: 1rem;
-	}
-
-	ul {
-		list-style: lower-roman;
 	}
 </style>
