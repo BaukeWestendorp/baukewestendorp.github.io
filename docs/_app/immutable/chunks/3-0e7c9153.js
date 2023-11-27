3d4459e44590c742c82e7f2b52b746a209f27ab6
BaukeWestendorp <baukewestendorp@gmail.com>
2023-11-28 00:00:53 +0100

    update about page

--- docs/_app/immutable/chunks/3-0e7c9153.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/over/_page.svelte-921cf767.js";export{t as component};
--- src/routes/over/+page.svelte
+++ src/routes/over/+page.svelte
@@ -11,10 +11,13 @@
 <div class="over">
 	<img src="{base}/pages/over/bauke.jpg" alt="Bauke" />
 	<p>
-		Je had hoogstwaarschijnlijk al zo'n vermoeden: ik ben Bauke Westendorp. Al heel mijn leven woon
-		in het weinigzijdige maar vertrouwde Drachten, alhoewel mijn vroegtijdige studentenhartje al een
-		tijdje oogt op het prachtige Groningen. Ik kan me onvoorwaardelijk vermaken met muziek, foto,
-		video, kunst en voornamelijk programmeren met een mespuntje zwarte peper.
+		Ja hoor! Ik ben het: Bauke Westendorp! Sinds ik ben begonnen aan mijn studie
+		<i>Communication & Multimedia Design</i>
+		aan de <i>Hanzehogeschool</i> (<a
+			href="https://www.hanze.nl/nl/opleidingen/voltijd/bachelor/communication-and-multimedia-design"
+			>"oh ja leuk zeg! En wat doe je bij die studie dan precies?"</a
+		>) woon ik mijn inmiddels favoriete stad Groningen. Ik kan me onvoorwaardelijk vermaken met
+		muziek, foto, video, kunst en voornamelijk programmeren met een mespuntje zwarte peper.
 	</p>
 </div>
 
