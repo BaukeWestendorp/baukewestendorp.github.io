c103f40baf8672c9ff751f6643b3128677740167
BaukeWestendorp <baukewestendorp@gmail.com>
2023-11-27 16:16:41 +0100

    add shebangs to helper scripts

--- convertimg.sh
+++ convertimg.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 magick $1 -resize 1024 static/posts/images/$2.jpg
--- resizeall.sh
+++ resizeall.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 mogrify -resize 1024x1024^ -gravity center -extent 1024x1024 -path static/posts/images static/posts/images/*.jpg
